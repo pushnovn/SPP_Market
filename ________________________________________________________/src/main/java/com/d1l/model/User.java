217b48c4d37f84f949e46bc5cbed4ca47f1a47d2
Lexachok <lexachok@yandex.ru>
2016-09-19 22:47:33 +0300

    Add basic models

---
+++ ________________________________________________________/src/main/java/com/d1l/model/User.java
@@ -0,0 +1,51 @@
+package com.d1l.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "marketservice.user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @ManyToOne(optional = false)
+    @JoinColumn(name="role_id")
+    private Role role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
